84907fe5f87bfa86eb10a0c4b681d64d01d7c549
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-02-04 17:30:39 -0700

    all apis pull all necesary information

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,16 @@
 //var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=metric&appid=9057e02af26cff387f193be4d1eee3ae";
 var cityNames = document.querySelector("#city-names");
+var placeEl = document.querySelector("#place");
+var tempEl = document.querySelector("#temp");
+var humidEl = document.querySelector("#humid");
+var windEl = document.querySelector("#wind");
+var uvEl = document.querySelector("#uv");
+
 var cityObject = JSON.parse(localStorage.getItem('city'))
 if (!cityObject) {
-  place.textContent = 'Choose a City to see weather';
+  placeEl.textContent = 'Choose a City to see weather';
 } else {
-  place.textContent = cityObject.place;
+  placeEl.textContent = cityObject.placeEl;
 }
 var getCityCord = function (event) {
   var cityLat = event.target.getAttribute('data-lat');
@@ -17,6 +23,15 @@ var getCityCord = function (event) {
   }
 };
 
+var getCityName = function (event) {
+  var cityId = event.target.getAttribute('data-id');
+  if (cityId) {
+    getName(cityId);
+  } else {
+    alert("error cant find id");
+  }
+}
+
 var getCityTemp = function (cityLat, cityLon) {
   var api = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=9057e02af26cff387f193be4d1eee3ae";
 
@@ -27,22 +42,38 @@ var getCityTemp = function (cityLat, cityLon) {
     .then(function (data) {
       console.log(data)
       console.log(data.daily[0].temp.day)
-      var city ={place: data.name, temp: data.daily[0].temp.day, humidity: data.daily[0].humidity, wind: data.daily.wind_speed};
-      console.log(city);
-      cityString = JSON.stringify(city);
+      var city ={temp: data.daily[0].temp.day, humidity: data.daily[0].humidity, wind: data.daily[0].wind_speed};
+
+      var cityString = JSON.stringify(city);
       localStorage.setItem('city', cityString);
       var cityObject = JSON.parse(localStorage.getItem('city'))
-      place.textContent = cityObject.place;
-
+      console.log(cityObject);
+      tempEl.textContent = "Temperature: " + cityObject.temp + "\u00B0F";
+      humidEl.textContent = "Humidity: " + cityObject.humidity + "%";
+      windEl.textContent = "Wind Speed: " + cityObject.wind + " MPH";
     })
 }
-var getCityName = function (event) {
-  var cityId = event.target.getAttribute('data-id');
 
+
+var getName = function (cityId) {
+  var apiName = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId + "&appid=9057e02af26cff387f193be4d1eee3ae"
+
+  fetch(apiName)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data.city.name);
+      var name = {name: data.city.name};
+      var nameString =JSON.stringify(name);
+      localStorage.setItem('name', nameString);
+      var nameObject = JSON.parse(localStorage.getItem('name'));
+      placeEl.textContent = nameObject.name;
+    })
 }
 
 var getUv = function (cityLat, cityLon) {
-  var apiUv = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=9057e02af26cff387f193be4d1eee3ae"
+  var apiUv = "https://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=9057e02af26cff387f193be4d1eee3ae"
 
   fetch(apiUv)
     .then(function (response) {
@@ -50,6 +81,12 @@ var getUv = function (cityLat, cityLon) {
     })
     .then(function (data) {
       console.log(data.value);
+      var index = {uv: data.value}
+      var indexString = JSON.stringify(index);
+      localStorage.setItem('index', indexString);
+      var indexObject = JSON.parse(localStorage.getItem('index'));
+      uvEl.textContent = "UV Index: " + indexObject.uv;
+
     })
 }
 
